75c985f5558ed5ade3d984206e6cd122dd049a9b
hayat-khanum92 <hayat.khanum92@gmail.com>
2021-12-29 23:07:12 +0530

    itZ final

--- routes/v1/auth.route.js
+++ routes/v1/auth.route.js
@@ -2,27 +2,27 @@ const express = require("express");
 const router = express.Router();
 const httpStatus = require("http-status");
 
-const tokenService=require('../../Services/token.service');
+const tokenService = require('../../Services/token.service');
 
 const User = require("../../model/User");
 
 const createUser = async (user) => {
     console.log(user)
-    let obj={};
-    obj.flag=true;
+    let obj = {};
+    obj.flag = true;
     if (!user.password) {
         // throw new Error('Password Required!');
-        obj.message="Password Required!";
-        obj.flag=false;
+        obj.message = "Password Required!";
+        obj.flag = false;
     }
     let check = await User.isEmailTaken(user.email);
     if (check) {
         // throw new Error("Email already taken!")
-        obj.message="Email already taken!";
-        obj.flag=false;
+        obj.message = "Email already taken!";
+        obj.flag = false;
     } else {
         let doc = await User.create(user);
-        obj.doc=doc;
+        obj.doc = doc;
         // return doc;
     }
     return obj;
@@ -31,9 +31,9 @@ const createUser = async (user) => {
 const register = async (req, res) => {
     let usr = await createUser(req.body);
     let token = await tokenService.generateAuthTokens(usr);
-    if(!usr.flag){
-        res.status(httpStatus.BAD_REQUEST).json({message:usr.message});
-    }else{
+    if (!usr.flag) {
+        res.status(httpStatus.BAD_REQUEST).json({ message: usr.message });
+    } else {
         res.status(httpStatus.CREATED).send({
             user: usr.doc,
             tokens: token
@@ -72,9 +72,15 @@ const loginUserWithEmailAndPassword = async (email, password) => {
 };
 
 const login = async (req, res) => {
-    let user = await loginUserWithEmailAndPassword(req.body.email, req.body.password);
-    let user_token = await tokenService.generateAuthTokens(user);
-    res.status(httpStatus.OK).send({ user: user, tokens: user_token })
+    try {
+        let user = await loginUserWithEmailAndPassword(req.body.email, req.body.password);
+        let user_token = await tokenService.generateAuthTokens(user);
+        res.status(httpStatus.OK).send({ user: user, tokens: user_token })
+    } catch (error) {
+        res.status(httpStatus.BAD_REQUEST).send(error.message);
+    }
+
+
 
 };
 
